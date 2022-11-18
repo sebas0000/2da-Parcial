4d7f6128baa6e6989f978987df115e91fc266f48
Sebastian Gomez Calvario <sgomez4@ucol.mx>
2022-11-18 11:30:54 -0600

    Examen
    Final

---
+++ Examen.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ordenar(int list[], int n);
+void cambio(int *x, int *y);
+
+
+int main()
+{
+    int n=0;
+    printf("Ingrese el cuantos numeros desea ordenar: ");
+    scanf("%d",&n);
+    int list [n];
+    for (int i=0;i<n;i++){
+        printf("Ingrese los numeros a ordenar: ");
+        scanf("%d",&list[i]);}
+    ordenar(list,n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",list[i]);
+    }
+    return 0;
+}
+void ordenar (int list[], int n) {
+    for (int i=0;i<n-1;i++) {
+    	 int min=i;
+		for (int j=i+1;j<n;j++) {
+			if (list[j]<list[min]) 
+				min=j;
+		}
+		if (min!=i) 
+			cambio(&list[min],&list[i]);
+	}
+}
+
+void cambio(int*x, int*y) {
+	int aux = *x;
+	*x = *y;
+	*y = aux;
+}
+
